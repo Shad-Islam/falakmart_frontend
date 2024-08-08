c049102c0bd8afab9563651c66dfc72dd2676fd6
Shad-islam <m.shadislam01@gmail.com>
2024-08-08 09:06:57 +0600

    complete header part

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import logo from "./assets/images/logo.png";
+import cartIcon from "./assets/icons/cart.png";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <header>
+          <img className="logo" src={logo} alt="Logo" />
+          <img className="cart_icon" src={cartIcon} alt="Logo" />
+        </header>
+      </div>
+    </>
+  );
 }
 
 export default App;
