7d0ef5f594dd939661c9f1da2bac142261495b72
Shad-islam <m.shadislam01@gmail.com>
2024-08-08 09:44:32 +0600

    Add nav

--- src/App.js
+++ src/App.js
@@ -6,11 +6,34 @@ function App() {
   return (
     <>
       <div className="container">
+        {/* header start */}
         <header>
           <img className="logo" src={logo} alt="Logo" />
           <img className="cart_icon" src={cartIcon} alt="Logo" />
         </header>
+        {/* header end */}
       </div>
+      {/* nav start */}
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">New Arrivals</a>
+          </li>
+          <li>
+            <a href="#">Trending</a>
+          </li>
+          <li>
+            <a href="#">All Brands</a>
+          </li>
+          <li>
+            <a href="#">All Catagories</a>
+          </li>
+        </ul>
+      </nav>
+      {/* nav end */}
     </>
   );
 }
