94ccbd231a95a0f0bd038bbc9722055d9486e1e7
Shad-islam <m.shadislam01@gmail.com>
2024-09-22 21:57:13 +0600

    try to fix the slider problem

--- src/pages/home/image Slider/ImageSlider.js
+++ src/pages/home/image Slider/ImageSlider.js
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from "react";
 import "./style/ImageSlider.css";
 
-const images = [
-  require("../../../assets/images/red.jpg"),
-  require("../../../assets/images/blue.jpg"),
-  require("../../../assets/images/black.webp"),
-];
+const red = require("../../../assets/images/red.jpg");
+const blue = require("../../../assets/images/blue.jpg");
+const black = require("../../../assets/images/black.webp");
 
+const images = [red, blue, black];
 function ImageSlider() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      setCurrentIndex(
+        (prevIndex) => {
+          if (prevIndex === images.length - 1) {
+            images.push(red);
+            images.push(blue);
+            images.push(black);
+            console.log(images.length);
+            
+            if (images.length === 12) {
+              images.shift();
+              images.shift();
+              images.shift();
+              return 0;
+            }
+          } else {
+            return prevIndex + 1;
+          }
+        }
+        // prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
     }, 2000);
 
