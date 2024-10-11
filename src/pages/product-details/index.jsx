ae594fbad75c637607abd8e45cbe98bce0a4a019
Mohsin <oxgmohsin@gmail.com>
2024-10-11 12:14:13 +0600

    Related Product section is added in the product details page

--- src/pages/product-details/index.jsx
+++ src/pages/product-details/index.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "../../components/Header";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import RelatedProducts from "./related-product/RelatedProducts";
 
 export default function PrductDetails() {
   return (
     <>
       <Header showArrowIcon={true} />
       <Navbar />
-      <h1>This is the Prduct Details Page</h1>
+      <div className="container">
+        <RelatedProducts />
+      </div>
+
       <Footer />
     </>
   );
